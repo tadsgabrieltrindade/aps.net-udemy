3c8ca67bf0e01d2f9f74be2d1705cdcdab36fb39
Gabriel Trindade <icandoitgrt@gmail.com>
2023-02-24 18:15:53 -0300

    First Controller and Razor pages tests

---
+++ CódigoFonte/CódigoFonte/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CódigoFonte.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CódigoFonte.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            ViewData["email"] = "contato.trindadegabriel@gmail.com";
+            ViewData["tipoDado"] =  "double is double, of couse!"
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
